6ddb5b57e55df2ee19980dc196809a6fab438334
Xrls-O <finnussy@gmail.com>
2025-01-17 03:00:29 -0500

    Create botConfig.js

---
+++ src/config/botConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    botName: 'MyBot',
+    defaultPrefix: '!',
+    supportServer: 'https://discord.gg/supportserver', // Reemplaza con tu enlace de soporte
+    ownerId: '123456789012345678', // Reemplaza con tu ID de usuario
+    activity: {
+        type: 'PLAYING', // Opciones: PLAYING, WATCHING, LISTENING
+        message: 'con comandos geniales',
+    },
+};
