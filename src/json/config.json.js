187c682bcf84f12a37132d08573f1aa45cd9f8de
Xrls-O <finnussy@gmail.com>
2025-01-17 03:57:02 -0500

    Create config.json.js

---
+++ src/json/config.json.js
@@ -0,0 +1,6 @@
+module.exports = {
+    maxRetryAttempts: 5, // Número máximo de intentos para reintentar operaciones fallidas
+    defaultLanguage: 'es', // Idioma por defecto del bot
+    logLevel: 'info', // Niveles: error, warn, info, debug
+    supportedCurrencies: ['USD', 'EUR', 'BTC'], // Monedas soportadas para comandos relacionados a criptomonedas
+};
