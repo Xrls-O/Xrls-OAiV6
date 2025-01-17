48a69bfd5fa1e7d6e3585cb5bbc7deba4a3daad0
Xrls-O <finnussy@gmail.com>
2025-01-17 04:01:28 -0500

    Create permission.json.js

---
+++ src/json/permission.json.js
@@ -0,0 +1,5 @@
+module.exports = {
+    adminCommands: ['ban', 'kick', 'registerApp', 'viewApplications'], // Comandos de administración
+    userCommands: ['help', 'ping', 'meme', 'rps', 'cryptoPrice'], // Comandos disponibles para todos
+    restrictedRoles: ['Guest', 'Muted'], // Roles que no pueden ejecutar ningún comando
+};
