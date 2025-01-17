bdbc00666b7ca6b53dc26f82172f9fac353ebf48
Xrls-O <finnussy@gmail.com>
2025-01-17 03:07:03 -0500

    Create webhooksConfig.js

---
+++ src/config/webhooksConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    errorWebhook: {
+        id: 'WEBHOOK_ID', // Reemplaza con el ID del webhook
+        token: 'WEBHOOK_TOKEN', // Reemplaza con el token del webhook
+    },
+    logWebhook: {
+        id: 'LOG_WEBHOOK_ID',
+        token: 'LOG_WEBHOOK_TOKEN',
+    },
+};
